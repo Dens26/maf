f5eb254998277897534efa18332291655da7e3a1
Denis Bekaert <denis.bekaert@live.fr>
2025-07-12 20:21:03 +0200

    update_07_12_01

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -9,41 +9,41 @@ const mailjet = Mailjet.apiConnect(
 
 export async function POST({ request }: APIContext) {
   const body = await request.json();
-
+  console.log(body);
   try {
-    const response = await mailjet.post('send', { version: 'v3.1' }).request({
-      Messages: [
-        {
-          From: {
-            Email: 'contact@mon-assistant-formalites.db-dev.fr',
-            Name: 'Mon Assistant Formalités',
-          },
-          To: [
-            {
-              Email: 'denis.bekaert@live.fr',
-              Name: 'Denis Bekaert',
-            },
-          ],
-          TemplateID: 7133670,
-          TemplateLanguage: true,
-          Subject: `Message de ${body.name} via le formulaire`,
-          Variables: {
-            name: body.name,
-            email: body.email,
-            phone: body.phone,
-            company: body.company,
-            hear_about: body['hear-about'],
-            interests: body.interests,
-            service: body.service,
-            message: body.message,
-          },
-        },
-      ],
-    });
+    // const response = await mailjet.post('send', { version: 'v3.1' }).request({
+    //   Messages: [
+    //     {
+    //       From: {
+    //         Email: 'contact@mon-assistant-formalites.db-dev.fr',
+    //         Name: 'Mon Assistant Formalités',
+    //       },
+    //       To: [
+    //         {
+    //           Email: 'denis.bekaert@live.fr',
+    //           Name: 'Denis Bekaert',
+    //         },
+    //       ],
+    //       TemplateID: 7133670,
+    //       TemplateLanguage: true,
+    //       Subject: `Message de ${body.name} via le formulaire`,
+    //       Variables: {
+    //         name: body.name,
+    //         email: body.email,
+    //         phone: body.phone,
+    //         company: body.company,
+    //         hear_about: body.hear_about,
+    //         interests: body.interests,
+    //         service: body.service,
+    //         message: body.message,
+    //       },
+    //     },
+    //   ],
+    // });
 
-    return new Response(JSON.stringify({ success: true }), {
-      status: 200,
-    });
+    // return new Response(JSON.stringify({ success: true }), {
+    //   status: 200,
+    // });
   } catch (error) {
     console.error('Erreur Mailjet:', error);
     return new Response(JSON.stringify({ error: 'Erreur serveur' }), {
